b1d2999d82f9f55266ddccfbf16e620a72175320
Brent Singleton <brent.singleton@gmail.com>
2024-05-17 23:03:00 -0700

    implemented rate limit class

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ import time
 class redis_rate_limiter:
     def __init__(self, redis_connection=redis.Redis(host='localhost', decode_responses=True), ):
         self.redis_instance =   redis_connection
-        self._REDIS_LIMIT_SCRIPT =  r.script_load('''
+        self._REDIS_LIMIT_SCRIPT =  self.redis_instance.script_load('''
 if tonumber(redis.call('GET', KEYS[1])) >= tonumber(KEYS[2]) then
     return redis.call('DECRBY', KEYS[1], KEYS[2])
 else 
@@ -23,7 +23,7 @@ end
         
         # Create a Redis Pipeline object ( executes as an atomic transaction on the instance ) and returns
         # a 0-indexed array of values which are the results of each of the executable components in the pipeline
-        redis_transaction = r.pipeline()
+        redis_transaction = self.redis_instance.pipeline()
 
         # Index 0 : set up quota if it doesn't already exist setting the expiry(ex, seconds) and capacity(value)
         redis_transaction.set(name=rate_limit_bucket, value=300000000000, nx=True, ex=10)
@@ -34,7 +34,7 @@ end
         # Index 2 : decrease quota by decrement scalar if capacity exists, or returns -1 to signal no capacity for this size request
         # Note that we have previously cached the script on the redis instance and are calling the script using the SHA returned during
         # caching.
-        redis_transaction.evalsha(_REDIS_LIMIT_SCRIPT, 2, rate_limit_bucket, decrement)
+        redis_transaction.evalsha(self._REDIS_LIMIT_SCRIPT, 2, rate_limit_bucket, decrement)
         
         # Index 3 : get remaining expiration time
         redis_transaction.ttl(rate_limit_bucket)
@@ -55,42 +55,42 @@ end
 
         return results
 
-    async def mock_cf_request(request_name, IsRateLimited=True, num_pages=5, queue_name='limit:openAI-<ptu_endpoint>:default'):
-        # # sleep random between 1 and 5s
-        # await asyncio.sleep(random.randint(1,3))
 
-        async with asyncio.TaskGroup() as tg:
-            # process each page
-            for i in range(1, num_pages+1):
-                tg.create_task(process_page(request_name, i, queue_name))
 
             # run
-    async def rate_limiter(queue_name, required_capacity):
+    async def wait_for_capacity(self, queue_name, required_capacity):
         # block execution chain until available capacity
-        dequeue = reserve_execution_capacity(rate_limit_bucket=queue_name, decrement=required_capacity)
+        dequeue = self.reserve_execution_capacity(rate_limit_bucket=queue_name, decrement=required_capacity)
         while int(dequeue['remaining_quota']) < 0:
             # print(f'Worker:{worker_name} sleeping for {dq['time_until_refresh']} seconds)')
             await asyncio.sleep(dequeue['time_until_refresh'])
-            dequeue = reserve_execution_capacity(rate_limit_bucket=queue_name, decrement=required_capacity)
+            dequeue = self.reserve_execution_capacity(rate_limit_bucket=queue_name, decrement=required_capacity)
 
-    async def process_page(worker_name, page_num, queue_name):
-        required_capacity = random.randint(1,5000)
-        await rate_limiter(queue_name, required_capacity)
+async def process_page(worker_name, page_num, queue_name, rate_limiter):
+    required_capacity = random.randint(1,5000)
+    await rate_limiter.wait_for_capacity(queue_name, required_capacity)
         # print(f'{datetime.datetime.now().strftime('%H:%M:%S.%f')} - API call for {worker_name} ( page {page_num} ( {required_capacity} tokens) ) started')
         
         # work takes fake amount of seconds between 1 and 5
         # await asyncio.sleep(random.randint(1,2))
         # print(f'API call for {worker_name} ( page {page_num} ) finished')
 
+async def mock_cf_request(request_name,rate_limiter,IsRateLimited=True, num_pages=5, queue_name='limit:openAI-<ptu_endpoint>:default'):
+    # # sleep random between 1 and 5s
+    # await asyncio.sleep(random.randint(1,3))
+
+    async with asyncio.TaskGroup() as tg:
+        # process each page
+        for i in range(1, num_pages+1):
+            tg.create_task(process_page(request_name, i, queue_name, rate_limiter))
 
 async def main():
-    global r
-    global 
+    rate_limiter = redis_rate_limiter()
 
     tStart = time.perf_counter_ns()
 
     async with asyncio.TaskGroup() as tg:
-        tg.create_task(mock_cf_request(request_name='Document1', queue_name=f'chr_ptu01', num_pages=2000)),
+        tg.create_task(mock_cf_request(request_name='Document1', rate_limiter=rate_limiter, queue_name=f'chr_ptu01', num_pages=2000)),
 
     tStop = time.perf_counter_ns()
 
